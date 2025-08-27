48ce2a104bd30fa940af4280e2b12d6944fcb6d6
ElStarko <114481973+ElStarko@users.noreply.github.com>
2025-08-27 09:35:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
